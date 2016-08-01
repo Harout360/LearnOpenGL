74addc4fe73be66ad0ab1b8b414dd7b98eab79c1
Grigoryan, Harout <haroutyun.grigoryan.864@my.csun.edu>
2016-08-01 16:37:36 -0700

    Initial

---
+++ app/src/main/java/com/natural/har8/learnopengl/OpenGLActivity.java
@@ -0,0 +1,45 @@
+package com.natural.har8.learnopengl;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenGLActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGLSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mGLSurfaceView = new GLSurfaceView(this);
+
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportsEs2) {
+            mGLSurfaceView.setEGLContextClientVersion(2);
+        } else {
+            return;
+        }
+
+        setContentView(R.layout.activity_open_gl);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+}
